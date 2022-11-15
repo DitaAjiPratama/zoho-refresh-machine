ce0b945df43ca88396b0ec313ca7e4a015d2f0e6
root <root@archi-main.asia-southeast2-a.c.archidistrict.internal>
2022-11-15 08:02:57 +0000

    Script to create access and script to refresh access

---
+++ access.py
@@ -0,0 +1,70 @@
+import mysql.connector as mariadb
+import requests
+import json
+
+con = {
+    'host'          : 'localhost',
+    'user'          : 'root',
+    'password'      : '',
+    'database'      : 'zrm',
+    'autocommit'    : True,
+}
+
+main_db = mariadb.connect(**con)
+cursor  = main_db.cursor()
+
+cursor.execute(f"SELECT `client_id`, `client_secret`, `redirect_uri`, `code` FROM `zoho` ")
+result          = cursor.fetchone()
+client_id       = result[0]
+client_secret   = result[1]
+redirect_uri    = result[2]
+code            = result[3]
+
+print(f"client_id: {client_id}")
+print(f"client_secret: {client_secret}")
+print(f"redirect_uri: {redirect_uri}")
+print(f"code: {code}")
+
+main_db.close()
+
+url             = "https://accounts.zoho.com/oauth/v2/token"
+payload         = {
+    "client_id"     : client_id,
+    "grant_type"    : "authorization_code",
+    "client_secret" : client_secret,
+    "redirect_uri"  : redirect_uri,
+    "code"          : code
+}
+response        = requests.post(url, data = payload)
+form_param      = json.loads(response.text)
+
+print("")
+print(form_param)
+print("")
+
+main_db = mariadb.connect(**con)
+cursor  = main_db.cursor()
+
+if "access_token"   in form_param:
+    access_token    = form_param["access_token"     ]
+    cursor.execute("ROLLBACK;"  )
+    cursor.execute("BEGIN;"     )
+    cursor.execute(f"UPDATE `zoho` SET `access_token` = '{access_token}', `when_update` = NOW() ")
+    # It could be dangerous in update query, because it don't have WHERE statement
+    cursor.execute("COMMIT;"    )
+if "refresh_token"  in form_param:
+    refresh_token   = form_param["refresh_token"    ]
+    cursor.execute("ROLLBACK;"  )
+    cursor.execute("BEGIN;"     )
+    cursor.execute(f"UPDATE `zoho` SET `refresh_token` = '{refresh_token}', `when_update` = NOW() ")
+    # It could be dangerous in update query, because it don't have WHERE statement
+    cursor.execute("COMMIT;"    )
+if "api_domain"     in form_param:
+    api_domain      = form_param["api_domain"       ]
+    cursor.execute("ROLLBACK;"  )
+    cursor.execute("BEGIN;"     )
+    cursor.execute(f"UPDATE `zoho` SET `api_domain` = '{api_domain}', `when_update` = NOW() ")
+    # It could be dangerous in update query, because it don't have WHERE statement
+    cursor.execute("COMMIT;"    )
+
+main_db.close()
---
+++ refresh.py
@@ -0,0 +1,92 @@
+import sys
+import time
+import datetime
+import mysql.connector as mariadb
+import requests
+import json
+
+
+
+print("""\033[32;1m
+  _____     _             ____       __               _       __  __            _     _            
+ |__  /___ | |__   ___   |  _ \ ___ / _|_ __ ___  ___| |__   |  \/  | __ _  ___| |__ (_)_ __   ___ 
+   / // _ \| '_ \ / _ \  | |_) / _ \ |_| '__/ _ \/ __| '_ \  | |\/| |/ _` |/ __| '_ \| | '_ \ / _ \\
+  / /| (_) | | | | (_) | |  _ <  __/  _| | |  __/\__ \ | | | | |  | | (_| | (__| | | | | | | |  __/
+ /____\___/|_| |_|\___/  |_| \_\___|_| |_|  \___||___/_| |_| |_|  |_|\__,_|\___|_| |_|_|_| |_|\___|
+                                                                                                   
+\033[m""")
+
+def refresh_token():
+    
+    con = {
+        'host'      : 'localhost',
+        'user'      : 'root',
+        'password'  : '',
+        'database'  : 'zrm',
+        'autocommit': True,
+    }
+    
+    main_db = mariadb.connect(**con)
+    cursor  = main_db.cursor()
+
+    cursor.execute(f"SELECT `client_id`, `client_secret`, `refresh_token` FROM `zoho` ")
+    result          = cursor.fetchone()
+    client_id       = result[0]
+    client_secret   = result[1]
+    refresh_token   = result[2]
+
+    # print("client_id:"      +client_id)
+    # print("client_secret:"  +client_secret)
+    # print("refresh_token:"  +refresh_token)
+
+    main_db.close()
+
+    url             = "https://accounts.zoho.com/oauth/v2/token"
+    payload         = {
+        "client_id"     : client_id,
+        "grant_type"    : "refresh_token",
+        "client_secret" : client_secret,
+        "refresh_token" : refresh_token
+    }
+    response        = requests.post(url, data = payload)
+    form_param      = json.loads(response.text)
+
+    # print("")
+    # print(form_param)
+    # print("")
+
+    main_db = mariadb.connect(**con)
+    cursor  = main_db.cursor()
+
+    if "access_token"   in form_param:
+        access_token    = form_param["access_token"     ]
+        cursor.execute("ROLLBACK;"  )
+        cursor.execute("BEGIN;"     )
+        cursor.execute(f"UPDATE `zoho` SET `access_token` = '{access_token}', `when_update` = NOW() ")
+        # It could be dangerous in update query, because it don't have WHERE statement
+        cursor.execute("COMMIT;"    )
+    if "refresh_token"  in form_param:
+        refresh_token   = form_param["refresh_token"    ]
+        cursor.execute("ROLLBACK;"  )
+        cursor.execute("BEGIN;"     )
+        cursor.execute(f"UPDATE `zoho` SET `refresh_token` = '{refresh_token}', `when_update` = NOW() ")
+        # It could be dangerous in update query, because it don't have WHERE statement
+        cursor.execute("COMMIT;"    )
+    if "api_domain"     in form_param:
+        api_domain      = form_param["api_domain"       ]
+        cursor.execute("ROLLBACK;"  )
+        cursor.execute("BEGIN;"     )
+        cursor.execute(f"UPDATE `zoho` SET `api_domain` = '{api_domain}', `when_update` = NOW() ")
+        # It could be dangerous in update query, because it don't have WHERE statement
+        cursor.execute("COMMIT;"    )
+
+    main_db.close()
+
+    if "error"          in form_param:
+        print(f"[\033[35;1m{datetime.datetime.now()}\033[m] Error: {form_param['error']}")
+
+    print(f"[\033[35;1m{datetime.datetime.now()}\033[m] Refresh")
+
+while True:
+    refresh_token()
+    time.sleep(int(sys.argv[1])) # Expired in 3600
